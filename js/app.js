c0a76887d8ecee41e8333610809b1aefcc5e2a3e
Wilmer Mendoza <wmendozar.oracle.g8one@gmail.com>
2025-03-05 22:00:24 -0500

    creando funcion adicionar item en la lista.

--- js/app.js
+++ js/app.js
@@ -1,55 +1,70 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let cantidadAmigosenLista=0;
-let indiceLista=0;
-let listaAmigos=[];
-let titulo1=document.querySelector("h1");
-titulo1.innerHTML="Juego del Amigo Secreto";
-let titulo2=document.querySelector("h2");
-titulo2.innerHTML="Introduce el nombre de tus amigos";
-function showMsg(etiqueta, texto) {
-   let etiquetaHTML
-}
-function agregaAlistaAmigos() {
-   let nombreAmigo=document.querySelector("#amigo").value;
-   console.log(nombreAmigo);
-   if (listaAmigos.includes(nombreAmigo)){
-    alert("nombre repetido ingrese el nombre mas un identificador");
-   }else{
-    listaAmigos.push(nombreAmigo);
-   }
-   console.log(listaAmigos);
-   document.getElementById("amigo").value="";
-   return;
-}
-function limpiarInput() {
-   document.querySelector('#amigo').value = '';
-}
-function asignarTextoElemento(elemento, texto) {
+let cantidadAmigosenLista = 0;
+let indiceLista = 0;
+let listaAmigos = [];
+
+function muestraMsgEtiqueta(elemento, texto) {
    let elementoHTML = document.querySelector(elemento);
    elementoHTML.innerHTML = texto;
    return;
 }
+
+muestraMsgEtiqueta("h1","Juego del Amigo Secreto ");
+muestraMsgEtiqueta("h2","Introduce el nombre de tus amigos");
+
+function limpiarInput() {
+   document.querySelector("#amigo").value = '';
+}
+
+function addItemLista(ntextoitem){
+   let nuevoItem=document.createElement('li');
+   let ul=document.getElementById("listaAmigoa");
+   let textoit=document.createTextNode(ntextoitem);
+   nuevoItem.appendChild(textoit);
+   ul.appendChild(nuevoItem);
+   console.log(nuevoItem);
+   limpiarInput();
+   return;
+}
+function agregaAlistaAmigos() {
+   let nombreAmigo = document.getElementById('amigo').value;
+   console.log(nombreAmigo);
+   if (nombreAmigo==="") {
+      muestraMsgEtiqueta("h2","Por favor introduce un nombre");
+   }else{
+      if (listaAmigos.includes(nombreAmigo)) {
+         muestraMsgEtiqueta("h2","nombre repetido ingrese el nombre mas un identificador");
+      } else {
+         listaAmigos.push(nombreAmigo);
+      }
+   }
+
+   console.log(listaAmigos);
+   addItemLista(nombreAmigo);
+   return;
+}
+
 function sorteaAmigoSecreto() {
-   cantidadAmigosenLista=listaAmigos.length
-   indiceLista =  Math.floor(Math.random()*cantidadAmigosenLista)+1;
-   amigoSecreto=listaAmigos[indiceLista];
+   cantidadAmigosenLista = listaAmigos.length
+   indiceLista = Math.floor(Math.random() * cantidadAmigosenLista) + 1;
+   amigoSecreto = listaAmigos[indiceLista];
 
    console.log(indiceLista);
    console.log(amigoSecreto);
-   //Si ya sorteamos todos los números
-   if (listaAmigos.length == cantidadAmigosenLista) {
-       asignarTextoElemento('p','Ya se sortearon todos los nombres de amigos');
-   } else {
-       //Si el numero generado está incluido en la lista 
-       if (listaAmigosSorteados.includes(amigoSecreto)) {
-           return generarNumeroSecreto();
-       } else {
-           listaNumerosSorteados.push(numeroGenerado);
-           return numeroGenerado;
-       }
-   }
+   // //Si ya sorteamos todos los números
+   // if (listaAmigos.length == cantidadAmigosenLista) {
+   //     asignarTextoElemento('p','Ya se sortearon todos los nombres de amigos');
+   // } else {
+   //     //Si el numero generado está incluido en la lista 
+   //     if (listaAmigosSorteados.includes(amigoSecreto)) {
+   //         return generarNumeroSecreto();
+   //     } else {
+   //         listaNumerosSorteados.push(numeroGenerado);
+   //         return numeroGenerado;
+   //     }
+   // }
+   return amigoSecreto
 }
 function validainputvacio() {
 
 }
-    
